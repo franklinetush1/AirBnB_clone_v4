259855629636c384bcd3e71d1901f0288fe9bb65
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-04 19:47:26 +0300

    Update 2-hbnb.js

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+  $('document').ready(function () {
+  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
+  $.get(url, function (response) {
+    if (response.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
+  
+  let amenities = {};
+
+  function updateAmenities() {
+    $('.amenities H4').text(Object.values(amenities).join(', '));
+  }
+
+  $('INPUT[type="checkbox"]').on('change', function () {
+    const dataId = $(this).attr('data-id');
+    const dataName = $(this).attr('data-name');
+
+    if ($(this).is(':checked')) {
+      amenities[dataId] = dataName;
+    } else {
+      delete amenities[dataId];
+    }
+
+    updateAmenities();
+  });
+});
