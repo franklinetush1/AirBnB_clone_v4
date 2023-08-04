c2c28a10ff7e678260b91c6e3788fc0498468d75
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-04 10:52:20 +0300

    Add cache_id

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -33,11 +33,11 @@ def hbnb():
 
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-
+    cache_id = (str(uuid.uuid4()))
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places, cache_id)
 
 
 if __name__ == "__main__":
