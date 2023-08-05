0912a538277e94be0fad783dd1fbf166b5257ac2
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-05 10:46:34 +0300

    Update 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -7,6 +7,43 @@ $(document).ready(function () {
       $('div#api_status').removeClass('available');
     }
   });
+
+$.post(api + ':5001/api/v1/places_search/', '{}', function (data) {
+  const $sectionPlaces = $('SECTION.places');
+  $sectionPlaces.empty();
+
+  $.each(data, function (index, place) {
+    const article = `<ARTICLE>
+                      <DIV class="title">
+                        <H2>${place.name}</H2>
+                        <DIV class="price_by_night">
+                          ${place.price_by_night}
+                        </DIV>
+                      </DIV>
+                      <DIV class="information">
+                        <DIV class="max_guest">
+                          <I class="fa fa-users fa-3x" aria-hidden="true"></I>
+                          </BR>
+                          ${place.max_guest} Guests
+                        </DIV>
+                        <DIV class="number_rooms">
+                          <I class="fa fa-bed fa-3x" aria-hidden="true"></I>
+                          </BR>
+                          ${place.number_rooms} Bedrooms
+                        </DIV>
+                        <DIV class="number_bathrooms">
+                          <I class="fa fa-bath fa-3x" aria-hidden="true"></I>
+                          </BR>
+                          ${place.number_bathrooms} Bathrooms
+                        </DIV>
+                      </DIV>
+                      <DIV class="description">
+                        ${place.description}
+                      </DIV>
+                    </ARTICLE>`;
+    $sectionPlaces.append(article);
+  	});
+	}, 'json');
   
   let amenities = {};
 
