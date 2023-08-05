4baa778b499e3442e308759ad2a7ec0f910730b5
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-08-05 19:40:14 +0300

    Update cache id

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -37,7 +37,7 @@ def hbnb():
     return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places, cache_id)
+                           places=places, cache_id=cache_id)
 
 
 if __name__ == "__main__":
